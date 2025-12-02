8077308d664ecb181faa566da629da2aba977c2d
Georgi <georgi.lomov@mail.ru>
2025-12-02 14:47:29 -0500

    reg1

--- tests/test_user.py
+++ tests/test_user.py
@@ -19,23 +19,23 @@ users = [
 ]
 
 def test_get_existed_user():
-    '''Получение существующего пользователя'''
     response = client.get("/api/v1/user", params={'email': users[0]['email']})
     assert response.status_code == 200
     assert response.json() == users[0]
 
 def test_get_unexisted_user():
-    '''Получение несуществующего пользователя'''
-    pass
+    response = client.get("/api/v1/user", params={'email': 'nonexistent@mail.com'})
+    assert response.status_code == 404
 
 def test_create_user_with_valid_email():
-    '''Создание пользователя с уникальной почтой'''
-    pass
+    response = client.post("/api/v1/user", json={'name': 'Anna', 'email': 'a.anna@mail.com'})
+    assert response.status_code == 201
+    assert isinstance(response.json(), int)
 
 def test_create_user_with_invalid_email():
-    '''Создание пользователя с почтой, которую использует другой пользователь'''
-    pass
+    response = client.post("/api/v1/user", json={'name': 'Ivan', 'email': users[0]['email']})
+    assert response.status_code == 409
 
 def test_delete_user():
-    '''Удаление пользователя'''
-    pass
+    response = client.delete("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 204
